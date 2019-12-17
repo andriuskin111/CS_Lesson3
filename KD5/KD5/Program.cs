2e9aaea1dbeda05e1aa2de3bf1fc45b0733f8a0d
andriuskin111 <andriuskin@gmail.com>
2019-12-17 20:48:28 +0200

    KD5_v02

--- KD5/KD5/Program.cs
+++ KD5/KD5/Program.cs
@@ -10,6 +10,21 @@ namespace KD5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite grupes pavadinima:");
+            string grupesPavadinimas = Console.ReadLine();
+
+            Console.WriteLine("Iveskite grupes nariu skaiciu:");
+            int nariuSkaicius = 0;
+
+            if(!int.TryParse(Console.ReadLine(), out nariuSkaicius))
+            {
+                Console.WriteLine("Ivestas nekorektiskas skaicius");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Grupes pavadinimas yra: {0} nariu skaicius yra: {1}", grupesPavadinimas, nariuSkaicius);
+            Console.ReadLine();
         }
     }
 }
