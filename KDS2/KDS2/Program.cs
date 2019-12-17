9807d113a7e8548c4649ba561cd61c0a6e9bcd04
andriuskin111 <andriuskin@gmail.com>
2019-12-17 20:04:36 +0200

    KDS2_v02

--- KDS2/KDS2/Program.cs
+++ KDS2/KDS2/Program.cs
@@ -10,6 +10,15 @@ namespace KDS2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite atstuma metrais");
+            double atstumasKm = Convert.ToDouble(Console.ReadLine()) / 1000;
+            Console.WriteLine("Iveskite laika skundem");
+            double laikasMin = Convert.ToDouble(Console.ReadLine()) / 3600;
+
+            double greitis = atstumasKm / laikasMin;
+
+            Console.WriteLine("Greitis Km/h: {0}", greitis );
+            Console.ReadLine();
         }
     }
 }
