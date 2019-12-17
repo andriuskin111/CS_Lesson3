8fa3de28a3aae974b463651a59e687a75bc370d8
andriuskin111 <andriuskin@gmail.com>
2019-12-17 21:31:27 +0200

    KD7_v02

--- KD7/KD7/Program.cs
+++ KD7/KD7/Program.cs
@@ -10,6 +10,38 @@ namespace KD7
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite grupes nariu skaiciu:");
+            int nariuSkaicius = 0;
+
+            if (!int.TryParse(Console.ReadLine(), out nariuSkaicius))
+            {
+                Console.WriteLine("Ivestas nekorektiskas skaicius");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            if (nariuSkaicius == 1)
+            {
+                Console.WriteLine("Tai solo atlikejas");
+            }
+            else if(nariuSkaicius == 2)
+            {
+                Console.WriteLine("Tai duetas");
+            }
+            else if((nariuSkaicius > 2) && (nariuSkaicius < 10))
+            {
+                Console.WriteLine("Tai ansamblis");
+            }
+            else if(nariuSkaicius > 10)
+            {
+                Console.WriteLine("Tai kamerinis ansamblis");
+            }
+            else
+            {
+                Console.WriteLine("Ivestas nekorektiskas skaicius");
+            }
+
+            Console.ReadLine();
         }
     }
 }
