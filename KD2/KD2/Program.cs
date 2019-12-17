70d2a09edda83a5878a6f257e5699aece5630a74
andriuskin111 <andriuskin@gmail.com>
2019-12-17 18:42:55 +0200

    kd_v02

--- KD2/KD2/Program.cs
+++ KD2/KD2/Program.cs
@@ -10,6 +10,16 @@ namespace KD2
     {
         static void Main(string[] args)
         {
+            double skaicius1 = 0.1 * 10;
+            double skaicius2 = 0;
+
+            for(int ctr = 0; ctr < 10; ctr++)
+            {
+                skaicius2 += 0.1;
+            }
+
+            Console.WriteLine("{0:R} = {1:R} : {2}", skaicius1, skaicius2, skaicius1==skaicius2);
+            Console.ReadLine();
         }
     }
 }
