face9908977e78648c07e8333a0a3a7663d2a493
andriuskin111 <andriuskin@gmail.com>
2019-12-17 20:33:37 +0200

    KDS3_v02

--- KDS3/KDS3/Program.cs
+++ KDS3/KDS3/Program.cs
@@ -10,6 +10,15 @@ namespace KDS3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite rutulio diametra");
+            double rutulioDiametras = Convert.ToDouble(Console.ReadLine());
+            double rutulioSpindulys = rutulioDiametras / 2;
+            double rutulioPlotas = 4 * Math.PI * Math.Sqrt(rutulioDiametras);
+            double rutulioTuris = (4 / 3) * Math.PI * Math.Pow(rutulioPlotas, 3);
+
+            Console.WriteLine("Rutulio plotas: {0}", rutulioPlotas);
+            Console.WriteLine("Rutulio plotas: {0}", rutulioTuris);
+            Console.ReadLine();
         }
     }
 }
