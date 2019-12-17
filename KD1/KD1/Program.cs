4cf9b17ff77d9353285263c64490b3c8c88eafb3
andriuskin111 <andriuskin@gmail.com>
2019-12-17 18:32:24 +0200

    KD1_v02

--- KD1/KD1/Program.cs
+++ KD1/KD1/Program.cs
@@ -10,6 +10,18 @@ namespace KD1
     {
         static void Main(string[] args)
         {
+            float skaiciusFloat = 1f / 3;
+            double skaiciusDouble = 1d / 3;
+
+            if (skaiciusFloat == skaiciusDouble)
+            {
+                Console.WriteLine("skaiciusFloat {0} yra lygus skaiciusDouble {1}", skaiciusFloat, skaiciusDouble);
+            }
+            else
+            {
+                Console.WriteLine("skaisiuFloat {0} nera lygus skaiciusDouble {1}", skaiciusFloat, skaiciusDouble);
+            }
+            Console.ReadLine();
         }
     }
 }
