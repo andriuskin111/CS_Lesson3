fb22c6f922b0110a7cd29c5f1369e3b843c113ac
andriuskin111 <andriuskin@gmail.com>
2019-12-17 19:12:53 +0200

    KD3_v02

--- KD3/KD3/Program.cs
+++ KD3/KD3/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace KD3
 {
+    class Human
+    {
+        public string Name;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Human human1 = new Human();
+            human1.Name = "Vasia";
+
+            Human human2 = human1;
+
+            Console.WriteLine("human1.Name: " + human1.Name);
+            Console.WriteLine("human2.Name: " + human2.Name);
+
+            human2.Name = "Petia";
+
+            Console.WriteLine("human1.Name: " + human1.Name);
+            Console.WriteLine("human2.Name: " + human2.Name);
+
+            Console.ReadLine();
         }
     }
 }
