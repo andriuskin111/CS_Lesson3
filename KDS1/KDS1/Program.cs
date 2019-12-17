baaf04303d0a79753da6620d683ea0829424f6a6
andriuskin111 <andriuskin@gmail.com>
2019-12-17 19:38:16 +0200

    KDS1_v02

--- KDS1/KDS1/Program.cs
+++ KDS1/KDS1/Program.cs
@@ -10,6 +10,15 @@ namespace KDS1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite pirma simboli");
+            char pirmasSimbolis = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Iveskite pirma simboli");
+            char antrasSimbolis = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Iveskite pirma simboli");
+            char treciasSimbolis = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Simboliai atvirkstine tvarka: {0} {1} {2}", treciasSimbolis, antrasSimbolis, pirmasSimbolis);
+            Console.ReadLine();
         }
     }
 }
